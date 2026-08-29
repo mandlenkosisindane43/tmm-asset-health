251f0efeeb42e804042aa520aabd51eef1ed2d9a
mandlenkosisindane43 <mandlenkosisindane43@gmail.com>
2026-08-29 13:48:45 +0200

    Use compatible Vinext app router handler

--- worker/index.ts
+++ worker/index.ts
@@ -1,6 +1,6 @@
 /** Cloudflare Worker entry point for TMM Asset Health. */
 import { handleImageOptimization, DEFAULT_DEVICE_SIZES, DEFAULT_IMAGE_SIZES } from "vinext/server/image-optimization";
-import handler from "vinext/server/fetch-handler";
+import handler from "vinext/server/app-router-entry";
 
 interface Env {
   ASSETS: Fetcher;
