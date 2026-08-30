2ebc2a1f7cb2b30c85f0420ef7b8741681a0491b
mandlenkosisindane43 <mandlenkosisindane43@gmail.com>
2026-08-30 18:21:29 +0200

    Use uploaded signature photo for quotation and licence

--- worker/owner-signature-clear.ts
+++ worker/owner-signature-clear.ts
@@ -1,6 +1,5 @@
-import { OWNER_SIGNATURE_SVG } from "./owner-signature-data";
+import { ownerSignaturePhotoDataUri } from "./owner-signature-photo";
 
 export function ownerSignatureClearDataUri() {
-  const fixed = OWNER_SIGNATURE_SVG.replace('<g fill="#173a79">', '<g fill="#173a79" fill-rule="evenodd">');
-  return `data:image/svg+xml;charset=utf-8,${encodeURIComponent(fixed)}`;
+  return ownerSignaturePhotoDataUri();
 }
