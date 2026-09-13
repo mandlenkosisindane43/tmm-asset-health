b473bc8cfff06977ac7a470d56023c0aca2584c0
mandlenkosisindane43 <mandlenkosisindane43@gmail.com>
2026-09-13 23:53:14 +0200

    Connect condition monitoring to OEM ingestion

--- worker/router-condition-monitoring.ts
+++ worker/router-condition-monitoring.ts
@@ -1,4 +1,4 @@
-import currentApp from "./router-telemetry-ready";
+import currentApp from "./router-telemetry-ingestion-v2";
 
 interface ExecutionContext { waitUntil(promise: Promise<unknown>): void; passThroughOnException(): void; }
 interface ScheduledController { scheduledTime:number; cron:string; noRetry():void; }
